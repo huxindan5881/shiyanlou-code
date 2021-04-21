bc772a690ca6025fd56ab797f30109057fcdb248
huxindan5881 <student@localhost.localdomain>
2021-04-21 16:19:23 +0800

    tiaozhan

---
+++ jump7.py
@@ -0,0 +1,26 @@
+mkdir Demo
+cd Demo
+gedit README.md
+(#输入'#Demo'保存退出)
+cat README.md
+git init
+ls -la
+cd .git
+ls
+cd /home/shiyanlou/Demo 
+git add README.md
+git status
+git config --global user.name "huxindan5881"
+git config --global user.email "321686374@qq.com"
+git commit -m "注释"
+git remote add 仓库小名默认origin即刚刚创建的github仓库链接 仓库链接
+git push origin master
+cd /home/shiyanlou/
+git clone 链接
+
+
+
+
+
+
+
